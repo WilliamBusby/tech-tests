55f9ac0080f08a192fbbb6194d9ae08268814104
William Busby <williambusby1999@gmail.com>
2022-04-15 11:12:30 +0100

    Completed challenge 3 with writing to file

--- 03-fizzbuzz-node/app.js
+++ 03-fizzbuzz-node/app.js
@@ -1,1 +1,41 @@
 // Fizzbuzz++ code
+// Using fs to write to files
+
+const fs = require('fs');
+
+// Prime number checking
+
+const checkPrimeNumber = (num) => {
+  for(let i = 2; i<=Math.sqrt(num); i++) {
+    if(num % i == 0) return false;
+  }
+  return num > 1;
+}
+
+// Check number is divisble by other number
+
+const checkDivisbleByNum = (num, divider) => {
+  return num % divider == 0;
+}
+
+const writeToFile = (text) =>{
+  fs.appendFileSync("./output.txt", text, err => {
+    if(err) {
+      console.log(err);
+      return;
+    }
+  });
+}
+
+const checkStringResult = (i) => {
+  if(checkPrimeNumber(i)) return "FIZZBUZZ++"; // Checks for prime number
+  // Check if divisble by 3, add "FIZZ" to string, if divisble by 5, add "BUZZ" to string
+  const fizzAndOrBuzz = (checkDivisbleByNum(i,3) ? "FIZZ" : "") + (checkDivisbleByNum(i,5) ? "BUZZ" : "");
+  // If fizzAndOrBuzz is empty, return i, otherwise return fizzAndOrBuzz
+  return (fizzAndOrBuzz) ? fizzAndOrBuzz : i;
+}
+
+// Run for nums 1 - 500
+for(let i = 1; i<=500; i++) {
+  writeToFile(checkStringResult(i) + "\n");
+}
